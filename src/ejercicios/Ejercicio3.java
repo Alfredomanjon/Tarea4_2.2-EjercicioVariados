06b98078b2f8b591312b43caba9ed575895e5187
Alfredo Manjon <manjon.canela@gmail.com>
2018-11-20 17:25:17 +0100

    Commit 7 Corregido el ejercicio 3  -5/25-

--- src/ejercicios/Ejercicio3.java
+++ src/ejercicios/Ejercicio3.java
@@ -5,14 +5,18 @@ import java.util.Scanner;
 public class Ejercicio3 {
 	public static void main(String[] args) {
 		int numero;
-		System.out.print("Introduce un numero entre 1-99:");
+		System.out.print("Introduce un numero:");
 		Scanner sc = new Scanner(System.in);
 		numero = sc.nextInt();
-		if (numero > 10) {
-			System.out.println("Intoduce un numero entre 1-99");
+		if (numero > 100) {
+			System.out.println("Introduce un numero entre 1-99");
+		}else if (numero <0) {
+			System.out.println("Introduce un numero entre 1-99");
 		}
-		else if (numero < 10) {
+		else if (numero > 10) {
 			System.out.println("El numero tiene 2 dígitos");
+		}else if (numero > 0) {
+			System.out.println("El numero tiene 1 dígitos");
 		}
 			
 		
---
+++ src/ejercicios/Ejercicio6.java
@@ -0,0 +1,18 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio6 {
+	public static void main(String[] args) {
+		int numero;
+		System.out.print("Introduce un numero entre 1-99:");
+		Scanner sc = new Scanner(System.in);
+		numero = sc.nextInt();
+		if (numero > 10) {
+			System.out.println("Intoduce un numero entre 1-99");
+		}
+		else if (numero < 10) {
+			System.out.println("El numero tiene 2 dígitos");
+		}
+	}
+}
