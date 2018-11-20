6643872ff9ffda9056c7a164247217cbc8a96b7a
Alfredo Manjon <manjon.canela@gmail.com>
2018-11-20 19:08:38 +0100

    Commit 17 Arreglado error en ejercicio 6  -13/25-

--- src/ejercicios/Ejercicio06.java
+++ src/ejercicios/Ejercicio06.java
@@ -11,7 +11,7 @@ public class Ejercicio06 {
 		if (numero <0) {
 			System.out.println("Introduce un numero entre 1-99");
 		}else if (numero > 1000) {
-			System.out.println("ERROR");
+			System.err.println("error");
 		}
 		else if (numero > 0 && numero < 10) {
 			System.out.println("El numero tiene 1 dígitos");
