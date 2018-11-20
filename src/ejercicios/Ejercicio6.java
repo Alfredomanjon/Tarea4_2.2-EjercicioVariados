6ea405c3a085425f6e135c06c28c81e37dcd2944
Alfredo Manjon <manjon.canela@gmail.com>
2018-11-20 17:32:28 +0100

    Commit 8 Añadido ejercicio 6  -6/25-

--- src/ejercicios/Ejercicio6.java
+++ src/ejercicios/Ejercicio6.java
@@ -8,11 +8,19 @@ public class Ejercicio6 {
 		System.out.print("Introduce un numero entre 1-99:");
 		Scanner sc = new Scanner(System.in);
 		numero = sc.nextInt();
-		if (numero > 10) {
-			System.out.println("Intoduce un numero entre 1-99");
+		if (numero <0) {
+			System.out.println("Introduce un numero entre 1-99");
+		}else if (numero > 1000) {
+			System.out.println("ERROR");
 		}
-		else if (numero < 10) {
+		else if (numero > 0 && numero < 10) {
+			System.out.println("El numero tiene 1 dígitos");
+		}
+		else if (numero > 10 && numero < 100) {
 			System.out.println("El numero tiene 2 dígitos");
+		}else if (numero > 100) {
+			System.out.println("El numero tiene 3 dígitos");
 		}
+		sc.close();
 	}
 }
