f038050ce1fa545c5ae1d9194b8ddca13520aeb6
Alfredo Manjon <manjon.canela@gmail.com>
2018-11-20 19:00:50 +0100

    Commit 16 Correccion ejercicio 8  -13/25-

--- src/ejercicios/Ejercicio08.java
+++ src/ejercicios/Ejercicio08.java
@@ -5,14 +5,19 @@ import java.util.Scanner;
 public class Ejercicio08 {
 	public static void main(String[] args) {
 		int dia;
-		System.out.print("Introduce un dia:");
+		int mes;
+		System.out.print("Introduce un dia: ");
 		Scanner sc = new Scanner(System.in);
 		dia = sc.nextInt();
-		if (dia == 25) {
+		System.out.print("Introduce un mes: ");
+		Scanner sc1 = new Scanner(System.in);
+		mes = sc.nextInt();
+		if (dia == 25 && mes == 12) {
 			System.out.println("¡Es navidad! Dameeeeee los regalos");
 		}else {
 			System.out.println("No es navidad");
 		}
 		sc.close();
+		sc1.close();
 	}
 }
