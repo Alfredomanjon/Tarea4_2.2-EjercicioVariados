a3ad3364e03611bba576529602c9947d62a5de96
Alfredo Manjon <manjon.canela@gmail.com>
2018-11-20 16:58:58 +0100

    Commit 4 Añadido ejercicio 3 -3/25-

--- src/ejercicios/Ejercicio3.java
+++ src/ejercicios/Ejercicio3.java
@@ -4,9 +4,19 @@ import java.util.Scanner;
 
 public class Ejercicio3 {
 	public static void main(String[] args) {
-		int numero = 0;
-		System.out.print("Introduce una direccion de email:");
+		int numero;
+		System.out.print("Introduce un numero entre 1-99:");
 		Scanner sc = new Scanner(System.in);
 		numero = sc.nextInt();
+		if (numero > 10) {
+			System.out.println("Intoduce un numero entre 1-99");
+		}
+		else if (numero < 10) {
+			System.out.println("El numero tiene 2 dígitos");
+		}
+			
+		
 	}
+
+
 }
