09a834c03573616384d4a0c5758b5c7a2769794a
Alfredo Manjon <manjon.canela@gmail.com>
2018-11-20 17:14:18 +0100

    Commit 6 Añadido el ejercicio 5  -5/25-

---
+++ src/ejercicios/Ejercicio5.java
@@ -0,0 +1,20 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio5 {
+	public static void main(String[] args) {
+		int numero=0;
+		System.out.print("Introduce un numero :");
+		Scanner sc = new Scanner(System.in);
+		numero = sc.nextInt();
+		if (numero>0) {
+			System.out.println("El numero " + numero + " es un numero positivo");
+		}else if ( numero < 0) {
+			System.out.println("El numero " + numero + " es un numero negativo");
+		}else {
+			System.out.println("El numero " + numero + " es un numero nulo");
+		}
+		sc.close();
+	}
+}
