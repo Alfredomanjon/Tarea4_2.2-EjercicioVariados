2a4fee12f04244c1ea363495223f94fe87d7a22e
Alfredo Manjon <manjon.canela@gmail.com>
2018-11-20 16:33:12 +0100

    Commit 2 Añadido ejercicio 2  -2/25-

--- src/ejercicios/Ejercicio02.java
+++ src/ejercicios/Ejercicio02.java
@@ -1,8 +1,30 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio02 {
 	public static void main(String[] args) {
-		int nota = 4;
+		int nota1 = 0;
+		int nota2 = 0;
+		int nota3 = 0;
+		System.out.print("Introduce una direccion de email:");
+		Scanner sc = new Scanner(System.in);
+		nota1 = sc.nextInt();
+		System.out.print("Introduce una direccion de email:");
+		Scanner sc1 = new Scanner(System.in);
+		nota2 = sc.nextInt();
+		System.out.print("Introduce una direccion de email:");
+		Scanner sc2 = new Scanner(System.in);
+		nota3 = sc.nextInt();
+		int promedio = (nota1 + nota2 + nota3)/3;
+		if(promedio >= 7) {
+			System.out.println("Promocionado");
+		}else {
+			System.out.println("No Promociona");
+		}
+		sc.close();
+		sc1.close();
+		sc2.close();
 		
 	}
 }
